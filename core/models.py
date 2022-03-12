97e63237b99630d8283e65c1148e983617a042a5
kurts <kurtse99@mail.ru>
2022-03-12 20:59:49 +0500

    #2 Create Dep and bind with Stud

--- core/models.py
+++ core/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 
+class Departament(models.Model):
+    name = models.CharField('Название', max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
 class Student(models.Model):
+    departament = models.ForeignKey('core.Departament', on_delete=models.CASCADE, null=True, blank=True)
     lastName = models.CharField('Фамилия', max_length=128)
     name = models.CharField('Имя', max_length=128)
     group = models.IntegerField('Номер группы')
